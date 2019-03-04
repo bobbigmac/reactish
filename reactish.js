82ea0a940113b2d8b886aff1a62886356cb5505b
Bob Davies <admin@bobbigmac.com>
2019-03-04 09:48:32 +0000

    Comments

--- reactish.js
+++ reactish.js
@@ -1,32 +1,30 @@
 
-Element.prototype.render = function(str) {
-	//Is it faster to maintain some diff, or just to clear all like this?
+Element.prototype.render = function(template) {
+	// Just clear everything
 	Array.from(this.children).forEach(x => this.removeChild(x));
 
+	// Generate child nodes from template
 	const appendChild = Element.prototype.appendChild;
-	const doc = (new DOMParser()).parseFromString(str, 'text/html');
-	// console.log(doc.body.children);
-	return Array.from(doc.body.children).map(
+	const subDoc = (new DOMParser()).parseFromString(template, 'text/html');
+	
+	// Append subDoc children to this element
+	return Array.from(subDoc.body.children).map(
 		(domNode,pos) => appendChild.call(this, domNode)
 	);
 };
 
 Element.prototype.reactish = function(props, templateFunc) {
-	var self = this;
-
 	var reactiveTrap = {
-		set: function(target, key, value) {
+		set: (target, key, value) => {
 			if(target[key] !== value) {
 				target[key] = value;
-				// console.log('rerendering', target);
-				self.render(templateFunc.call(target));
+				this.render(templateFunc.call(target));
 			}
 			return true;
 		}
 	};
 	
-	self.state = new Proxy(props, reactiveTrap);
-	// console.log('rendering', this.state);
+	this.state = new Proxy(props, reactiveTrap);
 	
-	self.render(templateFunc.call(props));
+	this.render(templateFunc.call(props));
 };
